4dcdf4f9ae02360ef76b4a1836b2e9a48e3f0cf9
jiuhao47 <jiangjunyan22@mails.ucas.ac.cn>
2024-12-17 16:13:44 +0800

    ADD Lab2 v6 6/20 passed

---
+++ Lab2/misc/input_test.c
@@ -0,0 +1,7 @@
+void main() {
+  int obc x[2] = {1, 2};
+  input();
+  output_var = x[input_var];
+  output();
+  // output_var = x[input_var];
+}
---
+++ Lab2/misc/local_var_test.c
@@ -0,0 +1,5 @@
+void main() {
+  int a = 1;
+  output_var = a;
+  output();
+}
